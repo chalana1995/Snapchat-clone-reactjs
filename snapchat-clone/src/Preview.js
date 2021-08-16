ca0591fe4a82de01211595ae5cbe33a02dde575e
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-16 20:41:35 +0530

    preview screen part done

--- snapchat-clone/src/Preview.js
+++ snapchat-clone/src/Preview.js
@@ -3,7 +3,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import './Preview.css';
-import CloseIcon from '@material-ui/icons/Close'
+import CloseIcon from '@material-ui/icons/Close';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
+import SendIcon from '@material-ui/icons/Send';
+
+
 
 function Preview() {
 
@@ -17,14 +27,31 @@ function Preview() {
         }
     }, [cameraImage, history]);
 
-    const closePreview =() => {
-       dispatch(resetCameraImage());
+    const closePreview = () => {
+        dispatch(resetCameraImage());
+    }
+
+    const sendPost = () => {
+        
     }
 
     return (
         <div className="preview">
-            <CloseIcon onClick={closePreview} className="preview__close" style={{height:50}}/>
+            <CloseIcon onClick={closePreview} className="preview__close" style={{ height: 50 }} />
+            <div className="preview__toolbarRight">
+                <TextFieldsIcon />
+                <CreateIcon />
+                <NoteIcon />
+                <MusicNoteIcon />
+                <AttachFileIcon />
+                <CropIcon />
+                <TimerIcon />
+            </div>
             <img src={cameraImage} />
+            <div onClick={sendPost} className="preview__footer">
+                <h2>Send Now</h2>
+                <SendIcon fontSize="small" className="preview__sendIcon" />
+            </div>
         </div>
     )
 }
